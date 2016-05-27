95028c733faedb6240e30daba20fd7641fe6225b
repixels <ehab.m.ashraf@repixels.co>
2016-05-27 21:14:43 +0200

    Facebook SDK v0.1

--- ZOBA/AppDelegate.swift
+++ ZOBA/AppDelegate.swift
@@ -10,6 +10,9 @@ import UIKit
 import CoreData
 import Onboard
 import CoreLocation
+import FBSDKCoreKit
+import FBSDKShareKit
+import FBSDKLoginKit
 
 let isFirstTime = "isFirstTime"
 
@@ -23,17 +26,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-            self.locationManager.delegate = self
-            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
-            self.locationManager.requestAlwaysAuthorization()
-            NSLog("Yalla")
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        if let window = window {
-            window.backgroundColor = UIColor.whiteColor()
-            window.rootViewController = generateOnBoardingWithImage()
-            window.makeKeyAndVisible()
-        }
-        return true
+//            self.locationManager.delegate = self
+//            self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//            self.locationManager.requestAlwaysAuthorization()
+//            NSLog("Yalla")
+//        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+//        if let window = window {
+//            window.backgroundColor = UIColor.whiteColor()
+//            window.rootViewController = generateOnBoardingWithImage()
+//            window.makeKeyAndVisible()
+//        }
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func application(application: UIApplication,
+                     openURL url: NSURL,
+                             sourceApplication: String?,
+                             annotation: AnyObject) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(
+            application,
+            openURL: url,
+            sourceApplication: sourceApplication,
+            annotation: annotation)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -52,6 +67,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
--- ZOBA/ViewController.swift
+++ ZOBA/ViewController.swift
@@ -7,17 +7,73 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKShareKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
+    let facebookReadPermissions = ["public_profile", "email", "user_friends"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+        }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.loginBehavior = FBSDKLoginBehavior.SystemAccount
+            loginView.delegate = self
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if ((error) != nil)
+        {
+            //handle error
+        } else {
+            returnUserData()
+        }
+    }
+    
+    func returnUserData()
+    {
+        
+        
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"id,interested_in,gender,birthday,email,age_range,name,picture.width(480).height(480)"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let id : NSString = result.valueForKey("id") as! String
+                print("User ID is: \(id)")
+                //etc...
+            }
+        })
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
     }
 
 
