997a7e47578d7fdac133bbc871a19fff9cb79e27
omima <omima.ibrahim@outlook.com>
2016-06-06 16:10:57 +0200

    add Vehicle in navigation

--- ZOBA/TimelineViewController.swift
+++ ZOBA/TimelineViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import BTNavigationDropdownMenu
 import ChameleonFramework
 
+
 class TimelineViewController: UIViewController {
 
       var menuView: BTNavigationDropdownMenu!
@@ -18,7 +19,7 @@ class TimelineViewController: UIViewController {
         super.viewDidLoad()
        // Do any additional setup after loading the view.
         
-        let items = ["Car one","car two","car two","car two"]
+        let items = ["Car one","car two","car two","car two" ,"Add Vehicles"]
 
         self.navigationController?.navigationBar.titleTextAttributes = [NSFontAttributeName : UIFont(name: "Continuum Medium", size: 22)! ,NSForegroundColorAttributeName: UIColor.whiteColor() ]
         
@@ -26,7 +27,7 @@ class TimelineViewController: UIViewController {
         self.navigationItem.titleView = menuView
         
         menuView = BTNavigationDropdownMenu(navigationController: self.navigationController, title: items[0], items: items)
-        menuView.cellHeight = 50
+        menuView.cellHeight = 40
         menuView.cellBackgroundColor = UIColor.flatWhiteColor()
         menuView.cellSelectionColor = UIColor.flatSandColor()
         menuView.keepSelectedCellColor = true
@@ -43,8 +44,21 @@ class TimelineViewController: UIViewController {
         menuView.maskBackgroundOpacity = 0.3
         menuView.checkMarkImage = nil
         
+        menuView.checkMarkImage = UIImage(named: "plus_icon")
+        
+        
         menuView.didSelectItemAtIndexHandler = {(indexPath: Int) -> () in
             print("Did select item at index: \(indexPath)")
+            
+            let itemCount = items.count - 1
+            
+            if indexPath == itemCount {
+
+                let AddFuelViewController = self.storyboard!.instantiateViewControllerWithIdentifier("home") as UIViewController
+                
+                self.navigationController!.pushViewController(AddFuelViewController, animated: true)
+            }else{
+                print(" out ")}
             //self.selectedCellLabel.text = items[indexPath]
         }
         
@@ -64,7 +78,11 @@ class TimelineViewController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+//        
+//        if condition {
+//            <#code#>
+//        }
     }
-    */
+ */
 
 }
