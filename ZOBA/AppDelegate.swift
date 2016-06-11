c515a93302b95aaa8dc574422e03bdef7ddbd815
repixels <ehab.m.ashraf@repixels.co>
2016-06-11 23:11:40 +0200

    Alamofire Network Indicator

--- ZOBA/AppDelegate.swift
+++ ZOBA/AppDelegate.swift
@@ -14,6 +14,7 @@ import FBSDKCoreKit
 import FBSDKShareKit
 import FBSDKLoginKit
 import SwiftyUserDefaults
+import AlamofireNetworkActivityIndicator
 
 let isFirstTime = "isFirstTime"
 
@@ -28,6 +29,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         
         //Set Managed Context
         SessionObjects.currentManageContext = self.managedObjectContext
+        
+        //AlamoFire Network Indicator
+        NetworkActivityIndicatorManager.sharedManager.isEnabled = true
+        NetworkActivityIndicatorManager.sharedManager.startDelay = 0
+        NetworkActivityIndicatorManager.sharedManager.completionDelay = 0.5
         
         // Override point for customization after application launch.
         
